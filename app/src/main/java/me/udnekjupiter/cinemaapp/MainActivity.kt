9b6fc7dae4c8d3e4fc135c4f670365a70cdc042c
JUP1T <stevevets1000000@gmail.com>
2025-02-24 21:55:05 +0300

    asynchronized tasks test

--- app/src/main/java/me/udnekjupiter/cinemaapp/MainActivity.kt
+++ app/src/main/java/me/udnekjupiter/cinemaapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import me.udnekjupiter.cinemaapp.film.FilmListTest
 import me.udnekjupiter.cinemaapp.ui.theme.CinemaAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +27,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CinemaAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android KED",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FilmListTest(listOf("1", "2", "3"))
             }
         }
     }
@@ -38,10 +41,46 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+
+//@Preview(
+//    showBackground = true,
+//    showSystemUi = true
+//)
+//@Composable
+//fun DynamicButtonPreview(){
+//    DynamicButton()
+//}
+
+
 @Composable
-fun GreetingPreview() {
-    CinemaAppTheme {
-        Greeting("Android")
-    }
+fun DynamicButton(modifier: Modifier = Modifier){
+    val count = remember{ mutableStateOf(0) }
+
+    Text(
+        text = "Clicks: ${count.value}",
+        modifier = modifier
+            .padding(20.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .shadow(
+                elevation = 1.dp,
+                shape = RoundedCornerShape(15.dp)
+            )
+            .background(color = Color(0xffd0d0d0))
+            .clickable(
+                onClick = { count.value += 1 }
+            )
+            .padding(15.dp)
+            .size(count.value.dp)
+    )
 }
+
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    CinemaAppTheme {
+//        Greeting("Android")
+//    }
+//}
---
+++ app/src/main/java/me/udnekjupiter/cinemaapp/test/TestArea.kt
@@ -0,0 +1,115 @@
+package me.udnekjupiter.cinemaapp.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.SurfaceCoroutineScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import me.udnekjupiter.cinemaapp.ui.theme.CinemaAppTheme
+import kotlin.reflect.KSuspendFunction1
+
+class TestActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CinemaAppTheme {
+                Test()
+            }
+        }
+    }
+}
+
+@Composable
+fun Test(modifier: Modifier = Modifier){
+    val coroutineScope = rememberCoroutineScope()
+    val counters = List(25) { remember { mutableStateOf(0) } }
+    var counter2 = remember { mutableStateOf(250_000) }
+
+    Column(modifier
+        .fillMaxWidth()
+        .background(
+            shape = RectangleShape,
+            color = Color(0xFFB6B6B6)
+        )
+    ){
+        for (i in 0..24){
+            TrackerLine(counters[i], ::asyncTask1, coroutineScope)
+        }
+    }
+}
+
+suspend fun asyncTask1(input: MutableState<Int>){
+    for (n in 0..250_000){
+        input.value += 1
+        delay(1000)
+    }
+}
+suspend fun asyncTask2(input: MutableState<Int>){
+    for (n in 0..250_000){
+        input.value -= 1
+        delay(1000)
+    }
+}
+
+@Composable
+fun TrackerLine(
+    trackingValue: MutableState<Int>,
+    task: KSuspendFunction1<MutableState<Int>, Unit>,
+    coroutineScope: CoroutineScope){
+    val buttonEnabled = remember { mutableStateOf(true) }
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .clickable(
+            enabled = buttonEnabled.value,
+            onClick = { coroutineScope.launch { task(trackingValue) } }
+        )
+        .border(
+            width = 2.dp,
+            shape = RectangleShape,
+            color = Color.Black
+        )
+    ){
+        Text(
+            text = trackingValue.value.toString(),
+            fontSize = 22.sp,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(5.dp)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun TrackerLinePreview(){
+    val coroutineScope = rememberCoroutineScope()
+
+    TrackerLine(remember { mutableStateOf(42) }, ::asyncTask1, coroutineScope)
+}
